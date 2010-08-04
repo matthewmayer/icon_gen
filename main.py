ee0970cc94465ffd880685dde8140f96beec08af
Matt Mayer <matt@matthewmayer.co.uk>
2010-08-04 12:34:12 +0800

    fix image generation, styling

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from google.appengine.api import images
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-class Greeting(db.Model):
+class IconSet(db.Model):
     iphone4_app = db.BlobProperty()
     iphone_app = db.BlobProperty()
     ipad_app = db.BlobProperty()
@@ -42,9 +42,11 @@ class MainPage(webapp.RequestHandler):
 
 class Image (webapp.RequestHandler):
     def get(self):
-        greeting = db.get(self.request.get("img_id"))
+        iconset = db.get(self.request.get("img_id"))
         img_type = self.request.get("type")
-        img = getattr(greeting, img_type)
+        img = getattr(iconset, img_type)
+        self.response.headers['Content-Type'] = "image/png"
+        
         if img:
             self.response.headers['Content-Type'] = "image/png"
             self.response.out.write(img)
@@ -57,39 +59,44 @@ class Image (webapp.RequestHandler):
 class Generate(webapp.RequestHandler):
     
     def resize(self, w, h):
-        return None#return images.resize(self.request.get("img"), w, h)
+        return images.resize(self.request.get("img"), w, h)
     
     def post(self):
 
         icon_types = [
-            {'type':'iphone_app' 	,'name':'iPhone'		,'size':57	,'filename':'icon.png'},
-            {'type':'iphone4_app' 	,'name':'iPhone 4'		,'size':114	,'filename':'icon@2x.png'},
-            {'type':'ipad_app' 		,'name':'iPad'			,'size':72	,'filename':'icon-72.png'},
-            {'type':'iphone_spot' 	,'name':'Spotlight iPhone'	,'size':29	,'filename':'spotlight.png'},
-            {'type':'iphone4_spot' 	,'name':'Spotlight iPhone 4'	,'size':58	,'filename':'spotlight@2x.png'},
-            {'type':'ipad_spot' 	,'name':'Spotlight iPad'	,'size':50	,'filename':'spotlight-ipad.png'},
-            {'type':'appstore' 		,'name':'Original' 		,'size':512	,'filename':'original.png'},
+            {'section':'Application icons'},
+            {'type':'iphone_app' 	,'name':'iPhone'		,'size':57	,'filename':'Icon.png'},
+            {'type':'iphone4_app' 	,'name':'iPhone 4'		,'size':114	,'filename':'Icon@2x.png'},
+            {'type':'ipad_app' 		,'name':'iPad'			,'size':72	,'filename':'Icon-72.png'},
+            {'section':'Search/settings icons'},
+            {'type':'iphone_spot' 	,'name':'Spotlight iPhone'	,'size':29	,'filename':'Icon-Small.png'},
+            {'type':'iphone4_spot' 	,'name':'Spotlight iPhone 4'	,'size':58	,'filename':'Icon-Small@2x.png'},
+            {'type':'ipad_spot' 	,'name':'Spotlight iPad'	,'size':50	,'filename':'Icon-Small-50.png'},
+            {'section':'Original'},
+            {'type':'appstore' 		,'name':'Original' 		,'size':512	,'filename':'Original512.png'},
 	 ]
 
 
 
-        g = Greeting()
-	g.put()     
-       
-	images = []
-	for genre in icon_types:
-		img = self.request.get("img")
-		size = genre['size']		
-		if (size<512):
-			try:
-				img = db.Blob(self.resize(size,size))
-			except Exception:
-				pass
-		setattr(g,genre['type'],img)
-		images.append(genre)
-	g.put()	
-	path = os.path.join(os.path.dirname(__file__), 'templates','generate.html')
-	self.response.out.write(template.render(path, {"images":images, "img_id":g.key()}))
+        g = IconSet()
+        g.put()     
+           
+        images = []
+        for genre in icon_types:
+            if 'section' in genre:
+                images.append(genre)
+            else:
+                img = self.request.get("img")
+                size = genre['size']		
+                if (size<512):
+                    img = db.Blob(self.resize(size,size))
+                else:
+                    img = db.Blob(img)
+                setattr(g,genre['type'],img)
+                images.append(genre)
+        g.put()	
+        path = os.path.join(os.path.dirname(__file__), 'templates','generate.html')
+        self.response.out.write(template.render(path, {"images":images, "img_id":g.key()}))
 
 
 
